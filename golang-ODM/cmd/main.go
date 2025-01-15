14b8bb540d8900981453de988906df7f76619eb2
Victor Sakamoto <victorysyama@gmail.com>
2025-01-15 11:41:28 -0300

    testing get configmap form kubectl

--- golang-ODM/cmd/main.go
+++ golang-ODM/cmd/main.go
@@ -5,6 +5,14 @@ import (
 	"fmt"
 	"io"
 	"os"
+
+	"context"
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 // type Conditionals struct {
@@ -44,13 +52,57 @@ func ConvertJSONToMapInterface(jsonPath string) (MapInterface []map[string]inter
 
 }
 
+func ConvertConfigMaptoMapInterface(configMapName string) (MapInterface []map[string]interface{}, err error) {
+
+	return MapInterface, nil
+}
+
 func main() {
 
-	MapInterface, err := ConvertJSONToMapInterface("..\\configs\\conditionalsTest.json")
+	// MapInterface, err := ConvertJSONToMapInterface("..\\configs\\conditionalsTest.json")
+	// if err != nil {
+	// 	fmt.Println("NOK")
+	// }
+	// fmt.Println("OK", MapInterface)
+
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
 	if err != nil {
-		fmt.Println("NOK")
+		panic(err.Error())
 	}
-	fmt.Println("OK", MapInterface)
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	for {
+		// get pods in all the namespaces by omitting namespace
+		// Or specify namespace to get pods in particular namespace
+		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+		// Examples for error handling:
+		// - Use helper functions e.g. errors.IsNotFound()
+		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+		_, err = clientset.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", metav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			fmt.Printf("Pod example-xxxxx not found in default namespace\n")
+		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+			fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
+		} else if err != nil {
+			panic(err.Error())
+		} else {
+			fmt.Printf("Found example-xxxxx pod in default namespace\n")
+		}
+
+		time.Sleep(10 * time.Second)
+	}
+
+	//name: mongo-config
+	// kubeconfig := "\\c\\Users\\victo\\.kube\\config"
 
 	// expressionStr := "cmapo1 < campo2"
 
